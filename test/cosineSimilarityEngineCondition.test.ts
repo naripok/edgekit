f7024df94d0e6fdac44f1139ccec9d1a84ea9b36
naripok <fernando.canteruccio@gmail.com>
2020-12-10 13:49:03 -0300

    Improved Cosine Similarity Condition tests for multiple audience vectors

--- test/cosineSimilarityEngineCondition.test.ts
+++ test/cosineSimilarityEngineCondition.test.ts
@@ -30,6 +30,39 @@ const cosineSimilarityCondition: EngineCondition = {
   ],
 };
 
+const multipleCosineSimilarityCondition: EngineCondition = {
+  filter: {
+    any: false,
+    queries: [
+      {
+        version: 1,
+        property: 'topicDist',
+        filterComparisonType: QueryFilterComparisonType.COSINE_SIMILARITY,
+        value: [{
+          vector: [0.4, 0.8, 0.3],
+          threshold: 0.99,
+        },
+        {
+          vector: [0.1, 0.2, 0.1],
+          threshold: 0.99,
+        }],
+      },
+    ],
+  },
+  rules: [
+    {
+      reducer: {
+        name: 'count',
+      },
+      matcher: {
+        name: 'ge',
+        args: 1,
+      },
+    },
+  ],
+};
+
+
 // Vector condition with a bumped version
 const cosineSimilarityConditionV2: EngineCondition = {
   filter: {
@@ -59,9 +92,9 @@ const cosineSimilarityConditionV2: EngineCondition = {
   ],
 };
 
-describe('CosineSimilarity condition test', () => {
-  describe('Vector condition', () => {
-    it('matches the page view if similar enough', async () => {
+describe('Cosine Similarity condition test', () => {
+  describe('Cosine Similarity condition', () => {
+    it('matches the page view if similar enough', () => {
       const conditions = [cosineSimilarityCondition];
 
       const pageViews = [
@@ -81,7 +114,7 @@ describe('CosineSimilarity condition test', () => {
       expect(result).toEqual(true);
     });
 
-    it('does not match the page view if not similar enough', async () => {
+    it('does not match the page view if not similar enough', () => {
       const conditions = [cosineSimilarityCondition];
 
       const pageViews = [
@@ -111,8 +144,8 @@ describe('CosineSimilarity condition test', () => {
     });
   });
 
-  describe('Vector condition with a bumped version', () => {
-    it('matches the page view if similar enough and has the same version', async () => {
+  describe('Cosine Similarity condition with a bumped version', () => {
+    it('matches the page view if similar enough and has the same version', () => {
       const conditions = [cosineSimilarityConditionV2];
 
       const pageViews = [
@@ -132,7 +165,7 @@ describe('CosineSimilarity condition test', () => {
       expect(result).toBe(true);
     });
 
-    it('does not match the page view if similar enough but does not have the same version', async () => {
+    it('does not match the page view if similar enough but does not have the same version', () => {
       const conditions = [cosineSimilarityConditionV2];
 
       const pageViews = [
@@ -152,4 +185,97 @@ describe('CosineSimilarity condition test', () => {
       expect(result).toBe(false);
     });
   });
+
+  describe('Cosine Similarity Condition with multiple audience vectors', () => {
+    it('matches the page view if similar enough with any audience vector', () => {
+      const conditions = [multipleCosineSimilarityCondition];
+
+      const pageViewsV0 = [
+        {
+          ts: 100,
+          features: {
+            topicDist: {
+              version: 1,
+              value: [0.4, 0.8, 0.3],
+            },
+          },
+        },
+      ];
+
+      expect(check(conditions, pageViewsV0)).toEqual(true);
+
+      const pageViewsV1 = [
+        {
+          ts: 100,
+          features: {
+            topicDist: {
+              version: 1,
+              value: [0.1, 0.2, 0.1],
+            },
+          },
+        },
+      ];
+
+      expect(check(conditions, pageViewsV1)).toEqual(true);
+
+    });
+
+    it('matches the page views if similar enough with all audience vector', () => {
+      const conditions = [multipleCosineSimilarityCondition];
+
+      const pageViews = [
+        {
+          ts: 100,
+          features: {
+            topicDist: {
+              version: 1,
+              value: [0.4, 0.8, 0.3],
+            },
+          },
+        },
+        {
+          ts: 101,
+          features: {
+            topicDist: {
+              version: 1,
+              value: [0.1, 0.2, 0.1],
+            },
+          },
+        },
+      ];
+
+      expect(check(conditions, pageViews)).toEqual(true);
+
+    });
+
+    it('does not match the page view if not similar enough', () => {
+      const conditions = [cosineSimilarityCondition];
+
+      const pageViews = [
+        {
+          ts: 100,
+          features: {
+            topicDist: {
+              version: 1,
+              value: [0.2, 0.8, 0.1],
+            },
+          },
+        },
+        {
+          ts: 101,
+          features: {
+            topicDist: {
+              version: 1,
+              value: [0.3, 0.8, 0.1],
+            },
+          },
+        },
+      ];
+
+      const result = check(conditions, pageViews);
+
+      expect(result).toEqual(false);
+
+    });
+  })
 });
